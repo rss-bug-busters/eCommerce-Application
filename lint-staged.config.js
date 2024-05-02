3a6e17652b9aa108765953aaec9485a08f07f76f
FYMG <34478249+FYMG@users.noreply.github.com>
2024-05-02 17:30:14 +0300

    chore: update lint-stage settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   '**/*.{js, jsx, ts, tsx, cjs, mjs}': (filenames) => `eslint ${filenames.join(' ')} --fix`,
-  '**/*.{json, html}': (filenames) => `prettier ${filenames.join(' ')} --write --ignore-path .gitignore`,
+  '**/*.{json, html, yml, md}': (filenames) => `prettier ${filenames.join(' ')} --write --ignore-path .gitignore`,
   '**/*.{css,scss,sass,less,styl,vue,svelte}': (filenames) => `stylelint ${filenames.join(' ')} --fix`,
 };
 
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -13,3 +13,4 @@ describe('App', () => {
     render(<App />);
   });
 });
+
