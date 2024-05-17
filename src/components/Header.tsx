219a85d92e90481bce34e75128e38d38db1f09cd
FYMG <svinpetr18@gmail.com>
2024-05-17 15:11:22 +0300

    feat: make header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const Header: FC = function () {
-  return <header>Header</header>;
+  return <header className="sticky z-50 bg-gray-300 top-0 p-4">Header</header>;
 };
 
 export default Header;
