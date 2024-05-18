266e19c6578ac4f57aad52f36afd7380a364a171
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-05-19 02:32:25 +0300

    feat: add current user state with logout in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
+import useCurrentUser from '@hooks/useCurrentUser';
+import useUserQueries from '@services/api/hooks/useUserQueries';
 import RoutePaths from '@utils/consts/RoutePaths';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Profile from '@assets/svg/profile.svg?react';
 
 const Header: FC = function () {
+  const { logout } = useUserQueries();
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const { isError, isSuccess, error, data } = useCurrentUser();
+
+  useEffect(() => {
+    if (isSuccess) {
+      setEmail(data.body.email);
+      setIsLogin(true);
+    }
+
+    if (isError && (error.statusCode === 403 || error.statusCode === 401)) {
+      setIsLogin(false);
+    }
+  }, [data, error, isError, isSuccess]);
+
   return (
-    <header className="sticky flex justify-between items-center bg-slate-50 z-50 top-0 p-4 border-b shadow-2xl">
-      <h1 className="text-3xl font-bold text-gray-700">eCommerce</h1>
+    <header className="sticky flex justify-between items-center bg-slate-50 z-50 top-0 p-2 border-b shadow-2xl">
+      <Link to={RoutePaths.MAIN}>
+        <h1 className="text-3xl font-bold text-gray-700">eCommerce</h1>
+      </Link>
       <div className="flex items-center justify-center gap-4">
         <Link to={RoutePaths.MAIN} className="font-bold text-blue-900 cursor-pointer">
           Home
@@ -17,17 +38,33 @@ const Header: FC = function () {
           About us
         </Link>
       </div>
-      <div className="flex items-center justify-end gap-4">
-        <Link
-          to={RoutePaths.REGISTRATION}
-          className="font-bold text-blue-900 cursor-pointer"
-        >
-          Register
-        </Link>
-        <Link to={RoutePaths.LOGIN} className="font-bold text-blue-900 cursor-pointer">
-          Log in
-        </Link>
-      </div>
+      {isLogin ? (
+        <div className="flex items-center justify-end gap-4">
+          <span>{email}</span>
+          <span>
+            <Profile />
+          </span>
+          <button
+            type="button"
+            className="font-bold text-blue-900 cursor-pointer"
+            onClick={() => logout().then(() => setIsLogin(false))}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center justify-end gap-4">
+          <Link
+            to={RoutePaths.REGISTRATION}
+            className="font-bold text-blue-900 cursor-pointer"
+          >
+            Register
+          </Link>
+          <Link to={RoutePaths.LOGIN} className="font-bold text-blue-900 cursor-pointer">
+            Log in
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
