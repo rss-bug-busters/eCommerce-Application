86302d54973b282b9ace38d13bd5d5152c98eeb1
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-05-18 14:34:11 +0200

    fix: delete special sign

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -8,7 +8,7 @@ const Login: FC = function () {
     <div className="flex flex-col items-center justify-center gap-3">
       <h1 className="text-4xl font-bold text-gray-700">Sign In</h1>
       <div className="flex items-center justify-center">
-        <span className="text-sm text-gray-500">Already have an account?</span>
+        <span className="text-sm text-gray-500">Dont have an account?</span>
         <Link
           to={RoutePaths.REGISTRATION}
           className="font-bold text-blue-900 ml-2 cursor-pointer"
--- src/components/Login/LoginForm/LoginForm.tsx
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -1,14 +1,17 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import InputField from '@components/ui/inputField/InputField';
 import { LoginFormSchema, LoginFormType } from './LoginValidation/LoginValidation.types';
+// import useUser from '@services/api/hooks/useUser';
 
 const onSubmit: SubmitHandler<LoginFormType> = async (data) => {
+  // useUser().login(data.email,data.password)
   console.log(data);
 };
 
 const LoginForm: FC = function () {
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -35,8 +38,16 @@ const LoginForm: FC = function () {
           register={register('password')}
           placeholder="Password"
           error={errors.password}
-          type="password"
+          type={showPassword ? 'text' : 'password'}
         />
+        <input
+          type="checkbox"
+          checked={showPassword}
+          onChange={() => setShowPassword((previous: boolean) => !previous)}
+          id="passwordShow"
+        />
+        {/* <label htmlFor="passwordShow">Show Password</label> */}
+        <span>Show Password</span>
       </div>
       <button
         type="submit"
