f298cf4f2deee103e3e508cc02123589ea24d824
FYMG <svinpetr18@gmail.com>
2024-05-21 13:44:53 +0300

    fix: fix types

--- src/utils/spa.ts
+++ src/utils/spa.ts
@@ -3,12 +3,12 @@ function spa(l: Location) {
     const decoded = l.search
       .slice(1)
       .split('&')
-      .map((s) => s.replaceAll('~and~', '&'))
+      .map((s): string => s.replaceAll('~and~', '&'))
       .join('?');
 
     window.history.replaceState(
       undefined,
-      undefined,
+      '',
       l.pathname.slice(0, -1) + decoded + l.hash
     );
   }
