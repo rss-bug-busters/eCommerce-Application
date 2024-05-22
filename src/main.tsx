8b1051004bcd17fcdd8a700c1e28134ee0d8960b
FYMG <svinpetr18@gmail.com>
2024-05-22 05:34:27 +0300

    fix: routing fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './tailwindcss.css';
-import spa from '@utils/spa';
 import Loader from '@components/ui/Loader/Loader';
-
-spa(window.location);
 
 const queryClient = new QueryClient();
 
