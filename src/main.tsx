d9b2f274c1bb10cd74d6ca3f2b53a56edb56abd9
FYM <34478249+FYMG@users.noreply.github.com>
2024-06-02 17:17:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.querySelector('#root')!).render(
     <ToastContainer
       stacked
       position="top-center"
-      autoClose={1000}
+      autoClose={2000}
       limit={2}
       hideProgressBar
       newestOnTop={false}
