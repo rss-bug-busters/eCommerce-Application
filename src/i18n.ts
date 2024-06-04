d854e2ec88192469aa671bf370cfe927fe1b3f3c
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-06-04 04:27:51 +0300

    fix: remove LanguageDetector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 
 export default i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -10,7 +10,7 @@ export default i18n
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
@@ -18,7 +18,7 @@ export default i18n
   .init({
     fallbackLng: 'en-US',
     debug: true,
-    supportedLngs: ['en-US', 'en'],
+    supportedLngs: ['en-US'],
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
