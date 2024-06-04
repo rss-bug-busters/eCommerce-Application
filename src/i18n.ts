e3e724a7ef5dd21437f1253b019552855b4b3141
FYM <34478249+FYMG@users.noreply.github.com>
2024-06-04 04:10:52 +0300

    fix: build error (#52)

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export default await i18n
+export default i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
   // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
