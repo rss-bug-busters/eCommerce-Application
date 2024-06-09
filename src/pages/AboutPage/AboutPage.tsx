e194db7ab4734cd709022f95768fa8a2314b85bf
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-06-09 22:17:49 +0200

    feat: add members generation

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,11 @@
+import About from '@components/About/about';
 import { FC } from 'react';
 
 const AboutPage: FC = function () {
   return (
     <div data-testid="about-page">
       <h1>AboutUs</h1>
+      <About />
     </div>
   );
 };
