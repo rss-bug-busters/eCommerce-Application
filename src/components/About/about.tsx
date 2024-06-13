4b0644222dfaf4bd508f79649ff0040afe066ee0
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-06-13 20:38:41 +0200

    refactor: refactor of usestate

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -4,7 +4,7 @@ import Contribution from './Contribution/Contribution';
 import Collaboration from './Collaboration/Collaboration';
 
 const About: FC = function () {
-  const [useSelected, setSelected] = useState(true);
+  const [isSelected, setSelected] = useState(true);
 
   return (
     <div className="mx-5 flex flex-col items-center justify-center">
@@ -15,20 +15,20 @@ const About: FC = function () {
           <div className="flex flex-wrap gap-3">
             <button
               type="button"
-              className={`flex items-end  rounded-md ${useSelected ? 'bg-gray-600' : 'bg-gray-800'} px-4 py-2 text-center font-semibold text-white hover:bg-gray-600 dark:bg-zinc-600 dark:hover:bg-zinc-50`}
+              className={`flex items-end  rounded-md ${isSelected ? 'bg-gray-600' : 'bg-gray-800'} px-4 py-2 text-center font-semibold text-white hover:bg-gray-600 dark:bg-zinc-600 dark:hover:bg-zinc-50`}
               onClick={() => setSelected(true)}
             >
               Contribution
             </button>
             <button
               type="button"
-              className={`flex items-end  rounded-md ${useSelected ? 'bg-gray-800' : 'bg-gray-600'} px-4 py-2 text-center font-semibold text-white hover:bg-gray-600 dark:bg-zinc-600 dark:hover:bg-zinc-50`}
+              className={`flex items-end  rounded-md ${isSelected ? 'bg-gray-800' : 'bg-gray-600'} px-4 py-2 text-center font-semibold text-white hover:bg-gray-600 dark:bg-zinc-600 dark:hover:bg-zinc-50`}
               onClick={() => setSelected(false)}
             >
               Collaboration
             </button>
           </div>
-          <div>{(useSelected && <Contribution />) || <Collaboration />}</div>
+          <div>{(isSelected && <Contribution />) || <Collaboration />}</div>
         </div>
         {/* ===================================== */}
         <TeamMembers />
