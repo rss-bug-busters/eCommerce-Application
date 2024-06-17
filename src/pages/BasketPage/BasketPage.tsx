2c794938ab1a6e48546247712be2460335612de9
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-06-18 02:17:09 +0300

    feat: add view for empty cart

--- src/pages/BasketPage/BasketPage.tsx
+++ src/pages/BasketPage/BasketPage.tsx
@@ -1,19 +1,44 @@
 import CartList from '@components/CartList/CartList';
 import CartTotals from '@components/CartTotals/CartTotals';
 import { FC } from 'react';
+import { useCart } from '@hooks/cart';
+import { Link } from 'react-router-dom';
+import RoutePaths from '@utils/consts/RoutePaths';
 
 const BasketPage: FC = function () {
+  const { data } = useCart();
+  const cart = data?.body;
+
+  // if (cart && cart.lineItems.length === 0) {
+  //   return <div data-testid="basket-page" className="container mx-auto p-4">
+  //
+  //   </div>;
+  // }
+
   return (
     <div data-testid="basket-page" className="container mx-auto p-4">
-      <h1 className="mb-4 text-center text-3xl font-bold">Your Cart</h1>
-      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-12">
-        <div className="mb-4 md:col-span-8">
-          <CartList />
+      <h1 className="mb-4 text-center text-3xl font-bold">
+        Your Cart
+        {cart?.lineItems.length === 0 && (
+          <span className="ml-2 text-cyan-900 dark:text-cyan-100">is empty</span>
+        )}
+      </h1>
+      {cart?.lineItems.length ? (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-12">
+          <div className="mb-4 md:col-span-8">
+            <CartList />
+          </div>
+          <div className="md:col-span-4">
+            <CartTotals />
+          </div>
         </div>
-        <div className="mb-4 md:col-span-4">
-          <CartTotals />
+      ) : (
+        <div className="mt-10 text-center">
+          <Link to={RoutePaths.CATALOG} className="button text-2xl font-semibold">
+            ← Go to catalog
+          </Link>
         </div>
-      </div>
+      )}
     </div>
   );
 };
