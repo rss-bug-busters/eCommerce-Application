bc2f85288a5b03b7b70b872dd151bdbd5bf6c7d0
Anton Ramarchuk <87061061+RemAntof@users.noreply.github.com>
2024-06-18 03:34:25 +0200

    about us (#68)
    * feat: add members representation
    
    * refactor: add discriptoin to all and fix wrapping
    
    * feat: add contribution and some text to collaboration
    
    * feat: add shadow to boxes
    
    * refactor: refactor of usestate
    
    * feat: add collaboration discription
    
    * feat: add RS logo link
    
    * refactor: move logo to the right
    
    * feat: update bio and make bio text align justify
    
    * refactor: move persons to consts, rename review issues
    
    ---------
    
    Co-authored-by: Anton Ramarchuk <anton.ramarchuk@sahaty.local>
    Co-authored-by: FYMG <svinpetr18@gmail.com>

---
+++ src/components/About/Collaboration/Collaboration.tsx
@@ -0,0 +1,93 @@
+import { FC } from 'react';
+
+const teamCollaboration = {
+  title: 'Team Collaboration 🤝',
+  description:
+    'In our React development team, collaboration is key. We maintain efficiency through regular weekly calls where we discuss progress, share advice, and distribute tasks.',
+  weeklyCalls: {
+    title: 'Weekly Calls',
+    items: [
+      'Review Progress: Discuss completed tasks and challenges.',
+      'Share Advice: Exchange insights and troubleshooting tips.',
+      'Distribute Issues: Assign tasks for the upcoming week based on expertise and availability.',
+    ],
+  },
+  collaborationPractices: {
+    title: 'Collaboration Practices 👥',
+    items: [
+      'Issue Tracking: Use tools like GitHub Issues for task management.',
+      'Code Reviews: Maintain code quality and share knowledge.',
+      'Pair Programming: Solve complex issues and foster team bonding.',
+      'Documentation: Ensure easy understanding and contribution to the project.',
+    ],
+  },
+  continuousImprovement: {
+    title: 'Continuous Improvement 📝',
+    items: [
+      'Conduct retrospective meetings after each sprint to reflect and improve collaboration strategies.',
+    ],
+  },
+  summary:
+    'Our team thrives on regular communication, mutual support, and organized task management, ensuring a productive and harmonious work environment.',
+};
+
+const Collaboration: FC = function () {
+  return (
+    <div className="mt-3 flex flex-col gap-3">
+      <h3 className="text-2xl font-bold text-gray-700 dark:text-white">Collaboration</h3>
+      <div className="flex flex-wrap place-content-around justify-start gap-5 rounded-lg bg-slate-100 p-6  shadow-[inset_0px_1px_52px_-20px_rgba(66,68,90,1)] ">
+        <div className="flex flex-wrap gap-3">
+          <div>
+            <h4 className="font-bold text-gray-700 dark:text-white">
+              {teamCollaboration.title}
+            </h4>
+            <p>{teamCollaboration.description}</p>
+          </div>
+          <section className="rounded-lg border-2 border-slate-300 p-2">
+            <h5 className="font-bold text-gray-700 dark:text-white">
+              {teamCollaboration.weeklyCalls.title}
+            </h5>
+            <ul className="mt-6 list-inside list-disc ">
+              {teamCollaboration.weeklyCalls.items.map((item) => (
+                <li key={`weeklyCalls-${Math.random().toString(36).slice(2, 9)}`}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="rounded-lg border-2 border-slate-300 p-2">
+            <h4 className="font-bold text-gray-700 dark:text-white">
+              {teamCollaboration.collaborationPractices.title}
+            </h4>
+            <ul className="mt-6 list-inside list-disc">
+              {teamCollaboration.collaborationPractices.items.map((item) => (
+                <li
+                  key={`collaborationPractices-${Math.random().toString(36).slice(2, 9)}`}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="rounded-lg border-2 border-slate-300 p-2">
+            <h4 className="font-bold text-gray-700 dark:text-white">
+              {teamCollaboration.continuousImprovement.title}
+            </h4>
+            <ul className="mt-6 list-inside list-disc">
+              {teamCollaboration.continuousImprovement.items.map((item) => (
+                <li
+                  key={`continuousImprovement-${Math.random().toString(36).slice(2, 9)}`}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </section>
+          <p>{teamCollaboration.summary}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collaboration;
---
+++ src/components/About/Contribution/Contribution.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import persons from '@utils/consts/persons';
+
+const Contribution: FC = function () {
+  return (
+    <div className="mt-3 flex flex-col gap-3">
+      <h3 className="text-2xl font-bold text-gray-700 dark:text-white">Contributions</h3>
+      <div className="flex w-full flex-wrap place-content-around gap-5 rounded-lg bg-slate-100  p-6 shadow-[inset_0px_1px_52px_-20px_rgba(66,68,90,1)]">
+        {persons.map((person) => (
+          <div
+            key={`person-contribution-${Math.random().toString(36).slice(2, 9)}`}
+            className="w max-w-lg flex-col rounded-lg border-slate-500 bg-white p-6 shadow-lg"
+          >
+            <img
+              className=" square-full absolute h-10 w-10 rounded-lg ring-2 ring-white"
+              src={person.photo}
+              alt="avatar"
+            />
+            <p className="start flex pl-12 text-center text-xl font-bold">
+              {person.name}
+            </p>
+            <ul className="mt-6 list-inside list-disc">
+              {person.contributions.map((contribution) => (
+                <li key={`contribution-${Math.random().toString(36).slice(2, 9)}`}>
+                  {contribution}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Contribution;
