99c0dc54a653c4236ba2b40bfd1b1859fe05c426
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-06-18 10:14:55 +0300

    refactor: change about page styles

--- src/components/About/Contribution/Contribution.tsx
+++ src/components/About/Contribution/Contribution.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react';
 import persons from '@utils/consts/persons';
+import clsx from 'clsx';
 
 const Contribution: FC = function () {
   return (
     <div className="mt-3 flex flex-col gap-3">
       <h3 className="text-2xl font-bold text-gray-700 dark:text-white">Contributions</h3>
-      <div className="flex w-full flex-wrap place-content-around gap-5 rounded-lg bg-slate-100  p-6 shadow-[inset_0px_1px_52px_-20px_rgba(66,68,90,1)]">
+      <div
+        className={clsx(
+          'flex w-full flex-wrap place-content-around gap-5 rounded-lg bg-slate-100 p-6',
+          'shadow-[inset_0px_1px_52px_-20px_rgba(66,68,90,1)]',
+          'dark:bg-zinc-900 dark:shadow-[inset_0px_1px_52px_-20px_rgba(223,222,221,1)]'
+        )}
+      >
         {persons.map((person) => (
           <div
             key={`person-contribution-${Math.random().toString(36).slice(2, 9)}`}
-            className="w max-w-lg flex-col rounded-lg border-slate-500 bg-white p-6 shadow-lg"
+            className="component-box flex max-w-lg flex-col"
           >
             <img
               className=" square-full absolute h-10 w-10 rounded-lg ring-2 ring-white"
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,62 +25,66 @@ const Header: FC = function () {
   }, [data, error, isError, isSuccess]);
 
   return (
-    <header className="container sticky top-0 z-10 flex flex-wrap items-center justify-between border-b bg-zinc-50 p-2 text-cyan-900 shadow-xl dark:border-none dark:bg-zinc-800 dark:text-white/75 dark:shadow-zinc-700/90">
-      <Link to={RoutePaths.MAIN}>
-        <div className="flex flex-row items-center text-cyan-900 hover:text-zinc-500 dark:text-current">
-          <BlachowoSVG className="w-22 h-12" />
-          {/* <h1 className="px-2 text-2xl font-bold ">Blachowo</h1> */}
+    <header className="sticky top-0 z-10 border-b bg-zinc-50 p-2 px-6 text-cyan-900 shadow-xl dark:border-none dark:bg-zinc-800 dark:text-white/75 dark:shadow-zinc-700/90">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <Link to={RoutePaths.MAIN}>
+          <div className="flex flex-row items-center text-cyan-900 hover:text-zinc-500 dark:text-current">
+            <BlachowoSVG className="w-22 h-12" />
+            {/* <h1 className="px-2 text-2xl font-bold ">Blachowo</h1> */}
+          </div>
+        </Link>
+        <div className="flex items-center justify-center gap-4">
+          <Link
+            to={RoutePaths.CATALOG}
+            className="cursor-pointer font-bold hover:underline"
+          >
+            Catalog
+          </Link>
+          <Link
+            to={RoutePaths.ABOUT}
+            className="cursor-pointer font-bold hover:underline"
+          >
+            About us
+          </Link>
         </div>
-      </Link>
-      <div className="flex items-center justify-center gap-4">
-        <Link to={RoutePaths.MAIN} className=" cursor-pointer font-bold hover:underline">
-          Home
-        </Link>
-        <Link
-          to={RoutePaths.CATALOG}
-          className="cursor-pointer font-bold hover:underline"
-        >
-          Catalog
-        </Link>
-        <Link to={RoutePaths.ABOUT} className="cursor-pointer font-bold hover:underline">
-          About us
-        </Link>
-      </div>
-      <div className="flex  items-center justify-end gap-4">
-        {isLogin ? (
-          <>
-            <Link className="flex gap-2" to={RoutePaths.PROFILE}>
-              <span className="cursor-pointer font-bold hover:underline">{userName}</span>
-              <span>
-                <Profile className="h-6 w-6" />
-              </span>
-            </Link>
+        <div className="flex  items-center justify-end gap-4">
+          {isLogin ? (
+            <>
+              <Link className="flex gap-2" to={RoutePaths.PROFILE}>
+                <span className="cursor-pointer font-bold hover:underline">
+                  {userName}
+                </span>
+                <span>
+                  <Profile className="h-6 w-6" />
+                </span>
+              </Link>
 
-            <button
-              type="button"
-              className="cursor-pointer font-bold hover:underline"
-              onClick={() => logout().then(() => setIsLogin(false))}
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link
-              to={RoutePaths.LOGIN}
-              className="cursor-pointer font-bold hover:underline"
-            >
-              Log in
-            </Link>
-            <Link
-              to={RoutePaths.REGISTRATION}
-              className="cursor-pointer font-bold hover:underline"
-            >
-              Sign up
-            </Link>
-          </>
-        )}
-        <BasketLink />
+              <button
+                type="button"
+                className="cursor-pointer font-bold hover:underline"
+                onClick={() => logout().then(() => setIsLogin(false))}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                to={RoutePaths.LOGIN}
+                className="cursor-pointer font-bold hover:underline"
+              >
+                Log in
+              </Link>
+              <Link
+                to={RoutePaths.REGISTRATION}
+                className="cursor-pointer font-bold hover:underline"
+              >
+                Sign up
+              </Link>
+            </>
+          )}
+          <BasketLink />
+        </div>
       </div>
     </header>
   );
