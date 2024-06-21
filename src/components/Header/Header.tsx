70b5b6eae6871ac0fa4c8886aadf7c8046813997
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-06-21 05:25:29 +0300

    feat: add user dropdown menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,12 @@
-import { useCurrentUser } from '@hooks/user';
-import { useUserQueries } from '@services/api/commercetools/hooks';
 import RoutePaths from '@utils/consts/RoutePaths';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import Profile from '@assets/svg/profile.svg?react';
 import BlachowoSVG from '@assets/svg/blachowo.svg?react';
 import BasketLink from '@components/Header/BasketLink/BasketLink';
 import ThemeSwitcher from '@components/Header/ThemeSwitcher/ThemeSwitcher';
+import UserDropdown from './UserDropdown/UserDropdown';
 
 const Header: FC = function () {
-  const { logout } = useUserQueries();
-  const [isLogin, setIsLogin] = useState<boolean>(false);
-  const [userName, setUserName] = useState<string>('');
-  const { isError, isSuccess, error, data } = useCurrentUser();
-
-  useEffect(() => {
-    if (isSuccess) {
-      setUserName(`${data.body.firstName} ${data.body.lastName?.slice(0, 1)}`);
-      setIsLogin(true);
-    }
-
-    if (isError && (error.statusCode === 403 || error.statusCode === 401)) {
-      setIsLogin(false);
-    }
-  }, [data, error, isError, isSuccess]);
-
   return (
     <header className="sticky top-0 z-10 border-b bg-zinc-50 p-2 px-6 text-cyan-900 shadow-xl dark:border-none dark:bg-zinc-800 dark:text-white/75 dark:shadow-zinc-700/90">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
@@ -35,56 +17,16 @@ const Header: FC = function () {
           </div>
         </Link>
         <div className="flex items-center justify-center gap-4">
-          <Link
-            to={RoutePaths.CATALOG}
-            className="cursor-pointer font-bold hover:underline"
-          >
+          <Link to={RoutePaths.CATALOG} className="navlink">
             Catalog
           </Link>
-          <Link
-            to={RoutePaths.ABOUT}
-            className="cursor-pointer font-bold hover:underline"
-          >
+          <Link to={RoutePaths.ABOUT} className="navlink">
             About us
           </Link>
         </div>
-        <div className="flex  items-center justify-end gap-4">
-          {isLogin ? (
-            <>
-              <Link className="flex gap-2" to={RoutePaths.PROFILE}>
-                <span className="cursor-pointer font-bold hover:underline">
-                  {userName}
-                </span>
-                <span>
-                  <Profile className="h-6 w-6" />
-                </span>
-              </Link>
-
-              <button
-                type="button"
-                className="cursor-pointer font-bold hover:underline"
-                onClick={() => logout().then(() => setIsLogin(false))}
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link
-                to={RoutePaths.LOGIN}
-                className="cursor-pointer font-bold hover:underline"
-              >
-                Log in
-              </Link>
-              <Link
-                to={RoutePaths.REGISTRATION}
-                className="cursor-pointer font-bold hover:underline"
-              >
-                Sign up
-              </Link>
-            </>
-          )}
+        <div className="flex items-center justify-end gap-4">
           <ThemeSwitcher />
+          <UserDropdown />
           <BasketLink />
         </div>
       </div>
